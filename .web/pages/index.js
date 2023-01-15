0e5bf0123703d619eb7ff2fd8baeb98cb255cf76
yoonhero <yoonhero06@naver.com>
2023-01-16 00:05:10 +0900

    feat: 1/15

---
+++ .web/pages/index.js
@@ -0,0 +1,67 @@
+import {useEffect, useRef, useState} from "react"
+import {useRouter} from "next/router"
+import {E, connect, updateState} from "/utils/state"
+import "focus-visible/dist/focus-visible"
+import {Button, Center, Text, VStack} from "@chakra-ui/react"
+import NextHead from "next/head"
+
+const EVENT = "ws://localhost:8000/event"
+export default function Component() {
+const [state, setState] = useState({"con": false, "finish_game": true, "game_start": 0, "game_start_time": null, "score": 0, "stop": false, "events": [{"name": "state.hydrate"}]})
+const [result, setResult] = useState({"state": null, "events": [], "processing": false})
+const router = useRouter()
+const socket = useRef(null)
+const { isReady } = router;
+const Event = events => setState({
+  ...state,
+  events: [...state.events, ...events],
+})
+useEffect(() => {
+  if(!isReady)
+    return;
+  if (!socket.current) {
+    connect(socket, state, result, setResult, router, EVENT)
+  }
+  const update = async () => {
+    if (result.state != null) {
+      setState({
+        ...result.state,
+        events: [...state.events, ...result.events],
+      })
+      setResult({
+        state: null,
+        events: [],
+        processing: false,
+      })
+    }
+    await updateState(state, result, setResult, router, socket.current)
+  }
+  update()
+})
+return (
+<Center sx={{"width": "100%", "height": "100vh"}}>
+{state.finish_game ? <VStack>
+<Text sx={{"fontSize": "60px"}}>
+{`Are You Ready?`}</Text>
+<Button onClick={() => Event([E("state.start_game", {})])}
+sx={{"bg": "#8DCBE6", "color": "#F7F5EB", "fontSize": "30px", "padding": "20px"}}>
+{`Yes`}</Button></VStack> : state.con ? <Center sx={{"width": "100%", "height": "100%"}}>
+{state.stop ? <VStack>
+<Text sx={{"fontSize": "200px"}}>
+{(state.score + "ms")}</Text>
+<Button onClick={() => Event([E("state.restart", {})])}>
+{`restart`}</Button></VStack> : <Button isActive={true}
+onClick={() => Event([E("state.stop_timer", {})])}
+sx={{"width": "100%", "height": "100%"}}>
+<Text sx={{"fontSize": "50px", "color": "#86C8BC"}}>
+{`Click!`}</Text></Button>}</Center> : <Center sx={{"width": "100%", "height": "100%"}}>
+<Text sx={{"fontSize": "50px"}}>
+{`Click When Text Gets Green!`}</Text></Center>}
+<NextHead>
+<title>{`Pynecone App`}</title>
+<meta content="A Pynecone app."
+name="description"/>
+<meta content="favicon.ico"
+property="og:image"/></NextHead></Center>
+)
+}
---
+++ your_reaction_time/your_reaction_time.py
@@ -0,0 +1,154 @@
+from pcconfig import config
+import pynecone as pc
+import time
+import random
+import asyncio
+
+
+class Score(pc.Model):
+    score: int
+    time: str
+
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+
+class State(pc.State):
+    finish_game: bool = True
+    game_start: int
+    stop: bool = False
+    score: int = 0
+
+    game_start_time: float = None
+
+    def stop_timer(self):
+        self.stop = True
+        now = current_milli_time()
+
+        self.score = now - self.game_start
+
+    def start_game(self):
+        self.finish_game = False
+        to_sleep = random.random() * 10
+
+        print(to_sleep)
+
+        self.game_start_time = current_milli_time() + to_sleep
+
+    @pc.var
+    def con(self) -> bool:
+        print("askdj;f")
+        if self.game_start_time == None:
+            return False
+        now = current_milli_time()
+        if now >= self.game_start_time:
+            print("??")
+            return True
+        else:
+            return False
+
+    async def tick(self):
+        await asyncio.sleep(1)
+        return self.tick
+
+    def restart(self):
+        self.finish_game = True
+        self.stop = False
+
+        self.start_game()
+
+# MODAL
+
+
+def start_game(State):
+    return pc.modal(
+        pc.modal_overlay(
+            pc.modal_content(
+                pc.center(
+                    pc.vstack(
+                        pc.modal_header(
+                            pc.heading("Your Reaction Time!")
+                        ),
+                        pc.modal_body(
+                            pc.button(
+                                "Are you Ready?",
+                                on_click=State.start_game,
+                                bg="#8DCBE6",
+                                # bg="rgb(239, 100, 49)",
+                                color="#F7F5EB"
+                            )
+                        )
+                    ),
+                    padding="30px"
+
+                )
+
+            ),
+
+        ),
+        is_open=State.finish_game,
+        border_radius="lg",
+    )
+
+
+def index():
+    return pc.center(
+        pc.cond(
+            State.finish_game,
+            pc.vstack(
+                pc.text("Are You Ready?", font_size="60px"),
+                pc.button(
+                    "Yes",
+                    on_click=State.start_game,
+                    bg="#8DCBE6",
+                    # bg="rgb(239, 100, 49)",
+                    color="#F7F5EB",
+                    font_size="30px",
+                    padding="20px"
+                ),
+            ),
+
+            pc.cond(
+                State.con,
+
+                pc.center(
+
+                    pc.cond(
+                        State.stop,
+                        pc.vstack(
+                            pc.text(State.score+"ms", font_size="200px"),
+                            pc.button("restart", on_click=State.restart)
+
+                        ),
+
+                        pc.button(
+                            pc.text("Click!",
+                                    font_size="50px", color="#86C8BC"),
+                            on_click=State.stop_timer,
+                            width="100%",
+                            height="100%",
+                            is_active=True,
+                        ),
+                    ),
+
+                    width="100%",
+                    height="100%",
+                ),
+                pc.center(
+                    pc.text("Click When Text Gets Green!",
+                            font_size="50px"),
+                    width="100%",
+                    height="100%",
+                )
+            ),
+        ),
+        width="100%",
+        height="100vh"
+    )
+
+
+# Add state and page to the app.
+app = pc.App(state=State)
+app.add_page(index)
+app.compile()
